575e00b300e6e2c097e2fde73d799d6420ec65a2
sebaez <baezsebastian20@gmail.com>
2024-09-12 20:05:00 -0300

    Refactorizar modelos para utilizar el nombre completo del modelo Usuario

--- administrador/models.py
+++ administrador/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 class Administrador(models.Model):
     usuario = models.ForeignKey('autenticacion.Usuario', on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=200)
     fecha_ingreso = models.DateField()
     estado = models.CharField(max_length=10, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')])
 
     def __str__(self):
-        return self.nombre + ' ' + self.apellidos
+        return f"{self.usuario.first_name} {self.usuario.last_name}"  # Nombre completo desde el modelo Usuario
--- terapeuta/models.py
+++ terapeuta/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 class Terapeuta(models.Model):
     usuario = models.ForeignKey('autenticacion.Usuario', on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=200)
     especialidad = models.CharField(max_length=100)
     fecha_ingreso = models.DateField()
     estado = models.CharField(max_length=10, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')])
 
     def __str__(self):
-        return self.nombre + ' ' + self.apellidos
+        return f"{self.usuario.first_name} {self.usuario.last_name}"  # Nombre completo desde el modelo Usuario
