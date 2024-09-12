fbb2aa1c896b0d2a2a174a3793277d783dbd0fbd
sebaez <baezsebastian20@gmail.com>
2024-09-12 20:00:59 -0300

    Modificación models.py de autenticacion

--- autenticacion/models.py
+++ autenticacion/models.py
@@ -9,4 +9,10 @@ class Usuario(AbstractUser):
     
     rol = models.CharField(max_length=20, choices=Roles.choices, default=Roles.RECEPCIONISTA)
     rut = models.CharField(max_length=12, unique=True)
-    cotrasena = models.CharField(max_length=100)
+
+    # Indicar que el campo rut será utilizado como el nombre de usuario
+    USERNAME_FIELD = 'rut'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'rol']  # Campos obligatorios adicionales
+
+    def __str__(self):
+        return self.rut  # Devolver el rut como representación del usuario
