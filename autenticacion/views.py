e88f6e00a5a4abf20dfaa5c8f45ad686635f7a9a
sebaez <baezsebastian20@gmail.com>
2024-09-12 18:24:48 -0300

    Restructurar la vista Login y adición de Redireccionamiento dependiendo del rol

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import FormView, RedirectView
+from django.shortcuts import redirect
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
-def login(request):
-    return render(request, 'login.html')
+class Login(FormView):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('redireccionamiento')
+
+    def form_valid(self, form):
+        usuario = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+        if usuario is not None:
+            login(self.request, usuario)
+            return redirect(self.get_success_url())
+        return super().form_valid(form)
+
+class Redireccionamiento(LoginRequiredMixin, RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        usuario = self.request.user
+        if usuario.rol == 'Administrador':
+            return reverse_lazy('gestion_terapeutas')
+        elif usuario.rol == 'Recepcionista':
+            return reverse_lazy('listar_terapeutas_activos')
+        elif usuario.rol == 'Terapeuta':
+            return reverse_lazy('agenda')
