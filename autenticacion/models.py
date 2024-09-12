1a0960427c15846be296c920fb58485f08755a05
sebaez <baezsebastian20@gmail.com>
2024-09-12 18:19:53 -0300

    Adición modelo usuario con rol, y los campos rut y email

--- autenticacion/models.py
+++ autenticacion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class Usuario(AbstractUser):
+    class Roles(models.TextChoices):
+        ADMINISTRADOR = 'Administrador'
+        RECEPCIONISTA = 'Recepcionista'
+        TERAPEUTA = 'Terapeuta'
+    
+    rol = models.CharField(max_length=20, choices=Roles.choices, default=Roles.RECEPCIONISTA)
+    rut = models.CharField(max_length=12, unique=True)
+    email = models.EmailField(unique=True)
