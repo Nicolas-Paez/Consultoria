61deafcfef3e842a2ff11b070e7626b29c63da00
sebaez <baezsebastian20@gmail.com>
2024-09-12 18:50:39 -0300

    Restructuracion del modelo de Administrador

--- administrador/models.py
+++ administrador/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Administrador(models.Model):
     nombre = models.CharField(max_length=100)
-    rut = models.CharField(max_length=12)
+    apellidos = models.CharField(max_length=200)
     fecha_ingreso = models.DateField()
     estado = models.CharField(max_length=10, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')])
 
     def __str__(self):
-        return self.nombre
+        return self.nombre + ' ' + self.apellidos
