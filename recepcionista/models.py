af7ac9c7310f8c08e23d51072749d2e6256ca527
sebaez <baezsebastian20@gmail.com>
2024-09-12 18:51:04 -0300

    Adición del modelo de Recepcionista

--- recepcionista/models.py
+++ recepcionista/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Recepcionista(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=200)
+    fecha_ingreso = models.DateField()
+    estado = models.CharField(max_length=10, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')])
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellidos
