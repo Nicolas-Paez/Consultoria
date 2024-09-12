2494f1c7b0501bb4407efdb75f41e774b1063dbb
sebaez <baezsebastian20@gmail.com>
2024-09-12 18:51:36 -0300

    Restructuración del modelo terapeuta y adicion de foreign key de usuario

--- terapeuta/models.py
+++ terapeuta/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Terapeuta(models.Model):
+    usuario = models.ForeignKey('autenticacion.Usuario', on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
-    rut = models.CharField(max_length=12)
+    apellidos = models.CharField(max_length=200)
     especialidad = models.CharField(max_length=100)
     fecha_ingreso = models.DateField()
     estado = models.CharField(max_length=10, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')])
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellidos
