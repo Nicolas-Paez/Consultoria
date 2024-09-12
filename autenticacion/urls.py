da39c9ae97ddeca22dfad40d91cf5625094bc0eb
sebaez <baezsebastian20@gmail.com>
2024-09-12 20:36:24 -0300

    Correción de urls.py

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from .views import Login, Redireccionamiento  # Importa las clases correctas
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('redireccionamiento/', views.Redireccionamiento, name='redireccionamiento'),
+    path('login/', Login.as_view(), name='login'),
+    path('redireccionamiento/', Redireccionamiento.as_view(), name='redireccionamiento'),
     path('', views.login, name='home'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
