6efdc590e94eb3587792308acd7d2f6122d2e75d
Sebastian <baezsebastian20@gmail.com>
2024-09-14 18:15:43 -0300

    Modificacion models.py de autenticacion, adicion de Profile

--- autenticacion/models.py
+++ autenticacion/models.py
@@ -1,18 +1,11 @@
+from django.contrib.auth.models import User, Group
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-class Usuario(AbstractUser):
-    class Roles(models.TextChoices):
-        ADMINISTRADOR = 'Administrador'
-        RECEPCIONISTA = 'Recepcionista'
-        TERAPEUTA = 'Terapeuta'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rut = models.CharField(max_length=12, unique=True)  # RUT del usuario
+    # El usuario puede tener múltiples roles
+    roles = models.ManyToManyField(Group, related_name="user_roles")
     
-    rol = models.CharField(max_length=20, choices=Roles.choices, default=Roles.RECEPCIONISTA)
-    rut = models.CharField(max_length=12, unique=True)
-
-    # Indicar que el campo rut será utilizado como el nombre de usuario
-    USERNAME_FIELD = 'rut'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'rol']  # Campos obligatorios adicionales
-
     def __str__(self):
-        return self.rut  # Devolver el rut como representación del usuario
+        return f'{self.user.username} ({self.rut})'
