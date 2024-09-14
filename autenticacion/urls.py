754c8c78a7657faff08277aea5956776d75ff8a2
Sebastian <baezsebastian20@gmail.com>
2024-09-14 19:04:22 -0300

    Refactorizar las URL de autenticación y agregar funcionalidad de selección de roles

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import Login, Redireccionamiento  # Importa las clases correctas
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', Login.as_view(), name='login'),
-    path('redireccionamiento/', Redireccionamiento.as_view(), name='redireccionamiento'),
+    path('login/', views.login, name='login'),
+    path('elegir_rol/', views.elegir_rol, name='elegir_rol'),
+    path('logout/', views.logout, name='logout'),
     path('', views.login, name='home'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
