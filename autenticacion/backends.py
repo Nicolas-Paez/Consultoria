f1ba80539f983a9a2cff8bacd817ff0e649e1842
Sebastian <baezsebastian20@gmail.com>
2024-09-14 18:21:16 -0300

    Creación de autenticacion personalizada para iniciar sesión con rut y contraseña a través de backend.py

---
+++ autenticacion/backends.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+class RutAuthBackend(ModelBackend):
+    def authenticate(self, request, rut=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(profile__rut=rut)  # Busca el usuario por el RUT
+            if user.check_password(password):  # Verifica la contraseña
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
