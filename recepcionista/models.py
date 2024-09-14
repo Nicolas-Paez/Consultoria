9735ae1b046afa45bbb8d3a68d5a75c97304379c
Sebastian <baezsebastian20@gmail.com>
2024-09-14 20:08:07 -0300

    Refactorizar la foreign key del usuario en archivos models.py

--- recepcionista/models.py
+++ recepcionista/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Recepcionista(models.Model):
-    usuario = models.ForeignKey('autenticacion.Usuario', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     fecha_ingreso = models.DateField()
     estado = models.CharField(max_length=10, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')])
 
     def __str__(self):
-        return f"{self.usuario.first_name} {self.usuario.last_name}"  # Nombre completo desde el modelo Usuario
+        return f"{self.user.first_name} {self.user.last_name}"
--- terapeuta/models.py
+++ terapeuta/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Terapeuta(models.Model):
-    usuario = models.ForeignKey('autenticacion.Usuario', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     especialidad = models.CharField(max_length=100)
     fecha_ingreso = models.DateField()
     estado = models.CharField(max_length=10, choices=[('Activo', 'Activo'), ('Inactivo', 'Inactivo')])
 
     def __str__(self):
-        return f"{self.usuario.first_name} {self.usuario.last_name}"  # Nombre completo desde el modelo Usuario
+        return f"{self.user.first_name} {self.user.last_name}"
