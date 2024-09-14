945d246774a938be865078ccb44f76819696d1a7
Sebastian <baezsebastian20@gmail.com>
2024-09-14 19:59:44 -0300

    Agregue el decorador @role_required para el control de acceso y el manejo de páginas a las que el usuario no tiene acceso.

--- administrador/views.py
+++ administrador/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from autenticacion.decorators import role_required
 
 # Create your views here.
+@role_required('Administrador')
 def gestion_terapeutas(request):
     return render(request, 'gestion_terapeutas.html')
