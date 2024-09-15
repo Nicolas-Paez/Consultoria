19f1e32739b0e198bd405f508e9b2f5b7d0dbd99
Sebastian <baezsebastian20@gmail.com>
2024-09-15 17:13:24 -0300

    Cambio en la inicialización de grupos, se hace después de cargar la aplicación, usando signals

--- autenticacion/admin.py
+++ autenticacion/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from .models import Profile
 
 # Registrar el modelo Profile en el panel de administración
@@ -7,12 +6,3 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'rut')
     search_fields = ('user__username', 'rut')
-
-# Llamar a la función para crear los grupos
-def create_groups():
-    Group.objects.get_or_create(name='Administrador')
-    Group.objects.get_or_create(name='Terapeuta')
-    Group.objects.get_or_create(name='Recepcionista')
-
-# Ejecutar la creación de grupos cuando se inicie el servidor
-create_groups()
