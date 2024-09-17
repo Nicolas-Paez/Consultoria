2d554fdb8873f4a6e5bc72669945d656436fdc23
Sebastian <baezsebastian20@gmail.com>
2024-09-16 22:21:02 -0300

    Refactorizar la longitud del campo RUT en el modelo 'Profile'

--- autenticacion/models.py
+++ autenticacion/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rut = models.CharField(max_length=12, unique=True)  # RUT del usuario
+    rut = models.CharField(max_length=10, unique=True)  # RUT del usuario
     
     def __str__(self):
         return f'{self.user.username} ({self.rut})'
