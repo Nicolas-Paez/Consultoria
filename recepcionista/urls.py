640f6e8302ea7bf53c8e1c84d1ec7967b4d37da7
crispardo2 <cristobal.pardo@cloud.uautonoma.cl>
2024-09-18 02:28:01 -0400

    formulario de agregar paciente (front)

--- recepcionista/urls.py
+++ recepcionista/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('recepcionista_pacientes_activos', views.recepcionista_pacientes_activos, name='recepcionista_pacientes_activos'),
     path('recepcionista_terapeutas_activos/', views.recepcionista_terapeutas_activos, name='recepcionista_terapeutas_activos'),
+    path('agregar_paciente', views.agregar_paciente, name='agregar_paciente')
 ]
