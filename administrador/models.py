6851e4bcfe4beb3aeeb799c9a4cc95b0dd5b5183
Sebastian <baezsebastian20@gmail.com>
2024-09-20 20:45:38 -0300

    Agregué migración del nuevo modelo 'Administrador' y modifique el campo 'departamento' para que se nullable

--- administrador/models.py
+++ administrador/models.py
@@ -3,7 +3,7 @@ from autenticacion.models import User
 class Administrador(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     fecha_contratacion = models.DateField()
-    departamento = models.CharField(max_length=100)
+    departamento = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
