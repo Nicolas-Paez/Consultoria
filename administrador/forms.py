2f416c9434c1672af653bfe4d57efc35261e1e68
Sebastian <baezsebastian20@gmail.com>
2024-09-21 13:19:57 -0300

    Refactoricé campos de formulario en administrador/forms.py para creación de terapeuta.
    - Se actualizaron los campos del formulario en la clase CrearTerapeutaForm para mejorar la experiencia del usuario y la validación de entradas.
    - Se agregó texto de marcador de posición y estilo a los campos del formulario.
    - Se cambiaron los tipos de widgets para los campos de fecha y hora.
    - Se actualizaron ModelChoiceFields para región, provincia y comuna para utilizar el conjunto de consultas correcto.

--- administrador/forms.py
+++ administrador/forms.py
@@ -7,31 +7,117 @@ from django.forms import inlineformset_factory
 
 class CrearTerapeutaForm(forms.ModelForm):
     # Campo del modelo 'Profile'
-    rut = forms.CharField(max_length=12, label='Rut', required=True)
+
+    rut = forms.CharField(
+        max_length=12,
+        label='Rut', 
+        required=True,
+        widget=forms.TextInput(attrs={'class':'campo-formulario','placeholder': 'Ej: XX.XXX.XXX-X'})
+    )
+
     # Campos del modelo 'User'
-    first_name = forms.CharField(max_length=150, label='Nombres', required=True)
-    last_name = forms.CharField(max_length=150, label='Apellidos', required=True)
-    email = forms.EmailField(label='Correo electrónico', required=True)
-    password = forms.CharField(widget=forms.PasswordInput, label='Contraseña', required=True)
+
+    first_name = forms.CharField(
+        max_length=150, 
+        label='Nombres', 
+        required=True,
+        widget=forms.TextInput(attrs={'class':'campo-formulario','placeholder': 'Ej: Juan Alberto'})
+    )
+    last_name = forms.CharField(
+        max_length=150, 
+        label='Apellidos', 
+        required=True,
+        widget=forms.TextInput(attrs={'class':'campo-formulario','placeholder': 'Ej: Pérez González'})
+    )
+    email = forms.EmailField(
+        label='Correo electrónico', 
+        required=True,
+        widget=forms.EmailInput(attrs={'class':'campo-formulario','placeholder': 'correoejemplo@ejemplos.com'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class':'campo-formulario','placeholder': 'Contraseña'}), 
+        label='Contraseña', 
+        required=True
+    )
+
     # Campos del modelo 'Profile'
-    telefono = forms.CharField(max_length=12, label='Teléfono', required=True)
-    fecha_nacimiento = forms.DateField(label='Fecha de nacimiento', required=True)
-    direccion = forms.CharField(max_length=255, label='Dirección', required=True)
-    
-    # Cambia los campos a ModelChoiceField
-    region = forms.ModelChoiceField(queryset=Region.objects.all(), label='Región', required=True)
-    provincia = forms.ModelChoiceField(queryset=Provincia.objects.none(), label='Provincia', required=True)  # Inicialmente vacío
-    comuna = forms.ModelChoiceField(queryset=Comuna.objects.none(), label='Comuna', required=True)  # Inicialmente vacío
-    
-    sexo = forms.ChoiceField(choices=(('M', 'Masculino'), ('F', 'Femenino')), label='Sexo', required=True)
-    
+
+    telefono = forms.CharField(
+        max_length=12, 
+        label='Teléfono', 
+        required=True,
+        widget=forms.TextInput(attrs={'class':'campo-formulario','placeholder': 'Ej: +56 9 1234 5678'})
+    )
+    fecha_nacimiento = forms.DateField(
+        label='Fecha de nacimiento', 
+        required=True,
+        widget=forms.DateInput(attrs={'class':'campo-formulario', 'type': 'date'})
+    )
+    direccion = forms.CharField(
+        max_length=255, 
+        label='Dirección', 
+        required=True,
+        widget=forms.TextInput(attrs={'class':'campo-formulario','placeholder': 'Ej: Calle Ejemplo 123'})
+    )
+    region = forms.ModelChoiceField(
+        queryset=Region.objects.all(), 
+        label='Región', 
+        required=True,
+        widget=forms.Select(attrs={'class':'campo-formulario'})
+    )
+    provincia = forms.ModelChoiceField(
+        queryset=Provincia.objects.none(), 
+        label='Provincia', 
+        required=True,
+        widget=forms.Select(attrs={'class':'campo-formulario'})
+    )
+    comuna = forms.ModelChoiceField(
+        queryset=Comuna.objects.none(), 
+        label='Comuna', 
+        required=True,
+        widget=forms.Select(attrs={'class':'campo-formulario'})
+    )
+    sexo = forms.ChoiceField(
+        choices=(('M', 'Masculino'), ('F', 'Femenino')), 
+        label='Sexo', 
+        required=True,
+        widget=forms.RadioSelect(attrs={'class': 'form-check-input'})
+    )
+
     # Campos del modelo 'Terapeuta'
-    especialidad = forms.CharField(max_length=100, label='Especialidad', required=True)
-    fecha_contratacion = forms.DateField(label='Fecha de contratación', required=True)
-    titulo = forms.CharField(max_length=100, label='Título', required=True)
-    experiencia = forms.IntegerField(label='Experiencia', required=True)
-    presentacion = forms.CharField(max_length=500, label='Presentación', required=False)
-    correo_contacto = forms.EmailField(label='Correo de contacto', required=False)
+    especialidad = forms.CharField(
+        max_length=100, 
+        label='Especialidad', 
+        required=True,
+        widget=forms.TextInput(attrs={'class':'campo-formulario', 'placeholder' :'Ej: Articulaciones Superiores'})
+    )
+    fecha_contratacion = forms.DateField(
+        label='Fecha de contratación', 
+        required=True,
+        widget=forms.DateInput(attrs={'class':'campo-formulario', 'type': 'date'})
+    )
+    titulo = forms.CharField(
+        max_length=100, 
+        label='Título', 
+        required=True,
+        widget=forms.TextInput(attrs={'class':'campo-formulario', 'placeholder': 'Ej: Licenciado en Terapia Ocupacional'})
+    )
+    experiencia = forms.IntegerField(
+        label='Experiencia', 
+        required=True,
+        widget=forms.NumberInput(attrs={'class':'campo-formulario'})
+    )
+    presentacion = forms.CharField(
+        max_length=500, 
+        label='Presentación', 
+        required=False,
+        widget=forms.Textarea(attrs={'class':'campo-formulario','rows': 3})
+    )
+    correo_contacto = forms.EmailField(
+        label='Correo de contacto', 
+        required=False,
+        widget=forms.EmailInput(attrs={'class':'campo-formulario','placeholder': 'correoejemplo@ejemplos.com'})
+    )
 
     class Meta:
         model = Terapeuta
@@ -55,13 +141,6 @@ class CrearTerapeutaForm(forms.ModelForm):
             'presentacion',
             'correo_contacto'
         ]
-
-        widgets = {
-            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
-            'fecha_contratacion': forms.DateInput(attrs={'type': 'date'}),
-            'sexo': forms.RadioSelect(attrs={'class': 'form-check-input'}),
-            'presentacion': forms.Textarea(attrs={'rows': 3}),
-        }
 
     def save(self, commit=True):
         # Creamos el usuario primero
@@ -127,10 +206,20 @@ class CrearTerapeutaForm(forms.ModelForm):
         elif self.instance.pk:
             self.fields['comuna'].queryset = self.instance.provincia.comuna_set.all()
 
+class HorarioForm(forms.ModelForm):
+    class Meta:
+        model = Horario
+        fields = ['dia', 'hora_inicio', 'hora_final']
+        widgets = {
+            'dia': forms.Select(attrs={'class':'seleccion'}),
+            'hora_inicio': forms.TimeInput(attrs={'class':'campo-formulario', 'type': 'time'}),
+            'hora_final': forms.TimeInput(attrs={'class':'campo-formulario', 'type': 'time'}),
+        }
+
 # Creamos el formset para los horarios del terapeuta (inlineformset)
 HorarioFormSet = inlineformset_factory(
     Terapeuta,
     Horario,
-    fields=('dia', 'hora_inicio', 'hora_final'),
+    form=HorarioForm,
     extra=7,  # 7 días de la semana, lunes a domingo
 )
