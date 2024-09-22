9bcd1014f8f82b4f6f8e67f51f5c9e7005999093
Sebastian <baezsebastian20@gmail.com>
2024-09-21 23:25:21 -0300

    Agregué validaciones para campos del formulario para crear terapeutas, además de implementar la asginación de  una contraseña automatica aleatoria y enviarla al correo del terapeuta creado.

--- administrador/forms.py
+++ administrador/forms.py
@@ -1,9 +1,21 @@
+from itertools import cycle
 from django import forms
 from django.contrib.auth.models import User, Group
 from autenticacion.models import Profile
 from terapeuta.models import Terapeuta, Horario
 from autenticacion.models import Region, Provincia, Comuna  # Asegúrate de importar tus modelos
 from django.forms import inlineformset_factory
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail # Importar la función send_mail, para enviar correos electrónicos
+from django.utils.crypto import get_random_string # Importar la función get_random_string, para generar una contraseña aleatoria
+import re
+from datetime import date
+from django.conf import settings
+
+def is_valid_email(text):
+        pattern = r'^[a-z][a-z0-9\-\.]+@[a-z]+\.[a-z]{1,3}$'
+
+        return re.search(pattern, text)
 
 class CrearTerapeutaForm(forms.ModelForm):
     # Campo del modelo 'Profile'
@@ -33,11 +45,6 @@ class CrearTerapeutaForm(forms.ModelForm):
         label='Correo electrónico', 
         required=True,
         widget=forms.EmailInput(attrs={'class':'campo-formulario','placeholder': 'correoejemplo@ejemplos.com'})
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class':'campo-formulario','placeholder': 'Contraseña'}), 
-        label='Contraseña', 
-        required=True
     )
 
     # Campos del modelo 'Profile'
@@ -85,6 +92,7 @@ class CrearTerapeutaForm(forms.ModelForm):
     )
 
     # Campos del modelo 'Terapeuta'
+
     especialidad = forms.CharField(
         max_length=100, 
         label='Especialidad', 
@@ -126,7 +134,6 @@ class CrearTerapeutaForm(forms.ModelForm):
             'first_name',
             'last_name',
             'email',
-            'password',
             'telefono',
             'fecha_nacimiento',
             'direccion',
@@ -143,14 +150,17 @@ class CrearTerapeutaForm(forms.ModelForm):
         ]
 
     def save(self, commit=True):
+        # Generar una contraseña aleatoria
+        password = get_random_string(length=12)
+
         # Creamos el usuario primero
         user = User.objects.create_user(
             username=self.cleaned_data['rut'],
             first_name=self.cleaned_data['first_name'],
             last_name=self.cleaned_data['last_name'],
             email=self.cleaned_data['email'],
+            password=password  # Usamos la contraseña generada
         )
-        user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
             
@@ -186,6 +196,17 @@ class CrearTerapeutaForm(forms.ModelForm):
         if commit:
             terapeuta.save()
         
+        # Enviar la contraseña por correo electrónico
+        send_mail(
+        'Bienvenido/a a la plataforma',
+        f'Estimado/a {self.cleaned_data["first_name"]}, su cuenta ha sido creada.\n'
+        f'Usuario: {self.cleaned_data["rut"]}\nContraseña: {password}\n'
+        'Le recomendamos cambiar su contraseña al ingresar a la plataforma.',
+        settings.DEFAULT_FROM_EMAIL,  # Usamos el emisor definido
+        [self.cleaned_data['email']],  # Destinatario
+        fail_silently=False,
+        )
+
         return terapeuta  # Retornamos el terapeuta creado
     
     def __init__(self, *args, **kwargs):
@@ -205,6 +226,135 @@ class CrearTerapeutaForm(forms.ModelForm):
                 pass  # Manejo de errores si la provincia no es válida
         elif self.instance.pk:
             self.fields['comuna'].queryset = self.instance.provincia.comuna_set.all()
+    
+    def clean_rut(self):
+        rut = self.cleaned_data.get('rut')
+
+        # Validación del formato
+        if not re.match(r'^\d{1,2}\.\d{3}\.\d{3}-[\dkK]$', rut):
+            raise forms.ValidationError('El RUT debe estar en el formato XX.XXX.XXX-X.')
+
+        # Se remueve el punto y el guión
+        clean_rut = rut.replace(".", "").replace("-", "")
+
+        # Se extrae la parte numérica y se verifica el dígito
+        num_part = clean_rut[:-1]
+        dv = clean_rut[-1].upper()
+
+        # Validación del dígito verificador
+        reversed_digits = map(int, reversed(num_part))
+        factors = cycle(range(2, 8))
+        s = sum(d * f for d, f in zip(reversed_digits, factors))
+        verificador = (-s) % 11
+        verificador = 'K' if verificador == 10 else str(verificador)
+        print("Dígito verificador del formulario:", dv)
+        print("Dígito verificador calculado:", verificador)
+
+        # Validación del dígito verificador
+        if dv != verificador:
+            raise forms.ValidationError('El dígito verificador del RUT no es válido.')
+
+        # Verificar si ya existe otro usuario con este RUT, excluyendo al usuario que estamos editando
+        if Profile.objects.filter(rut=rut).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('Ya existe un usuario con este RUT.')
+
+        return rut
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        # Permitir hasta 3 nombres separados por espacios
+        if not re.match(r'^[a-zA-Z]+( [a-zA-Z]+){0,2}$', first_name):
+            raise forms.ValidationError('El nombre solo puede contener letras y hasta 3 nombres separados por espacios.')
+        
+        return first_name
+    
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        # Permitir hasta 2 apellidos separados por un espacio
+        if not re.match(r'^[a-zA-Z]+( [a-zA-Z]+)?$', last_name):
+            raise forms.ValidationError('El apellido solo puede contener letras y hasta 2 apellidos separados por un espacio.')
+        
+        return last_name
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not is_valid_email(email):
+            raise forms.ValidationError('Por favor, ingrese una dirección de correo electrónico válida.')
+
+        # Verificar si ya existe otro User con este email, excluyendo al User que estamos editando
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+            raise forms.ValidationError('Ya existe un usuario con este correo electrónico.')
+
+        return email
+    
+    def clean_telefono(self):
+        telefono = self.cleaned_data['telefono']
+        # Validar el formato del teléfono con una expresión regular
+        if not re.match(r'^\+\d{2} \d \d{4} \d{4}$', telefono):
+            raise forms.ValidationError('El formato del teléfono debe ser: +56 9 1234 5678')
+        
+        return telefono
+    
+    def clean_fecha_nacimiento(self):
+        fecha_nacimiento = self.cleaned_data['fecha_nacimiento']
+        # Verificar que la fecha de nacimiento sea anterior a la fecha actual
+        if fecha_nacimiento >= date.today():
+            raise forms.ValidationError('La fecha de nacimiento debe ser anterior a la fecha actual.')
+        
+        return fecha_nacimiento
+    
+    def clean_direccion(self):
+        direccion = self.cleaned_data['direccion']
+        if not re.match(r'^[a-zA-Z\sáéíóúÁÉÍÓÚ]+ \d+$', direccion):
+            raise forms.ValidationError('La dirección debe seguir el formato "Nombre de la calle número". Por ejemplo, "Los Alamos 999".')
+        
+        return direccion
+    
+    def clean_especialidad(self):
+        especialidad = self.cleaned_data['especialidad']
+        if not re.match(r'^[a-zA-Z\sáéíóúÁÉÍÓÚ]+$', especialidad):
+            raise forms.ValidationError('La especialidad solo puede contener letras y espacios.')
+        
+        return especialidad
+    
+    def clean_fecha_contratacion(self):
+        fecha_contratacion = self.cleaned_data['fecha_contratacion']
+        # Verificar que la fecha de contratación sea anterior a la fecha actual
+        if fecha_contratacion >= date.today():
+            raise forms.ValidationError('La fecha de contratación debe ser anterior a la fecha actual.')
+        
+        return fecha_contratacion
+    
+    def clean_titulo(self):
+        titulo = self.cleaned_data['titulo']
+        if not re.match(r'^[a-zA-Z\sáéíóúÁÉÍÓÚ]+$', titulo):
+            raise forms.ValidationError('El título solo puede contener letras y espacios.')
+        
+        return titulo
+    
+    def clean_experiencia(self):
+        experiencia = self.cleaned_data['experiencia']
+        # Verificar que la experiencia sea un número positivo
+        if experiencia <= 0:
+            raise forms.ValidationError('La experiencia debe ser un número positivo o igual a 0.')
+        
+        return experiencia
+    
+    def clean_presentacion(self):
+        presentacion = self.cleaned_data.get('presentacion')
+        if not re.match(r'^[a-zA-Z0-9\sáéíóúÁÉÍÓÚ.,;:¡!¿?()"\']+$', presentacion):
+            raise forms.ValidationError('La presentación solo puede contener letras, números y signos de puntuación.')
+        
+        return presentacion
+    
+    def clean_correo_contacto(self):
+        correo_contacto = self.cleaned_data.get('correo_contacto')
+        if correo_contacto and not is_valid_email(correo_contacto):
+            raise forms.ValidationError('Por favor, ingrese una dirección de correo electrónico válida para el contacto.')
+
+        return correo_contacto
+    
+
 
 class HorarioForm(forms.ModelForm):
     class Meta:
@@ -215,6 +365,16 @@ class HorarioForm(forms.ModelForm):
             'hora_inicio': forms.TimeInput(attrs={'class':'campo-formulario', 'type': 'time'}),
             'hora_final': forms.TimeInput(attrs={'class':'campo-formulario', 'type': 'time'}),
         }
+    def clean(self):
+        cleaned_data = super().clean()
+        hora_inicio = cleaned_data.get('hora_inicio')
+        hora_final = cleaned_data.get('hora_final')
+
+        # Verificar que la hora de inicio sea anterior a la hora final
+        if hora_inicio and hora_final and hora_inicio >= hora_final:
+            raise ValidationError('La hora de inicio debe ser anterior a la hora final.')
+
+        return cleaned_data
 
 # Creamos el formset para los horarios del terapeuta (inlineformset)
 HorarioFormSet = inlineformset_factory(
