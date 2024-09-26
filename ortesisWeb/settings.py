0fe9002f7ddf5de0c1e7832b65d647578d57716f
crispardo2 <cristobal.pardo@cloud.uautonoma.cl>
2024-09-25 23:55:56 -0400

    Arreglo de Detalles en Agendar Cita Administrador

--- ortesisWeb/settings.py
+++ ortesisWeb/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "ortesisweb",
         "USER": "postgres",
-        "PASSWORD": "Admin123",
+        "PASSWORD": "Falaciado1#.",
         "PORT": "5432",
     }
 }
